379ae834c2ffe637fa06b87edde8112c8fcd254c
jdesiderio <desiderio.janyce@gmail.com>
2023-12-15 16:43:42 +0100

    add script for reduplicated code

---
+++ assets/js/skills.js
@@ -0,0 +1,27 @@
+const skills = [
+  { class: "html", icon: "fa-brands fa-html5", text: "HTML5" },
+  { class: "css", icon: "fa-brands fa-css3-alt", text: "CSS3" },
+  { class: "sass", icon: "fa-brands fa-sass", text: "Sass" },
+  { class: "javascript", icon: "fa-brands fa-square-js", text: "Javascript" },
+  { class: "react", icon: "fa-brands fa-react", text: "React" },
+  { class: "seo", icon: "fa-solid fa-globe", text: "SEO" }
+];
+
+const container = document.querySelector('.skillset');
+
+skills.forEach(skill => {
+  const skillDiv = document.createElement('div');
+  const bubbleDiv = document.createElement('div');
+  bubbleDiv.className = `${skill.class} bubble`;
+
+  const icon = document.createElement('i');
+  icon.className = `${skill.icon} fa-2xl`;
+  bubbleDiv.appendChild(icon);
+
+  const textP = document.createElement('p');
+  textP.textContent = skill.text;
+
+  skillDiv.appendChild(bubbleDiv);
+  skillDiv.appendChild(textP);
+  container.appendChild(skillDiv);
+});
